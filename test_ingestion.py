11a9493f9571f9cc3db8fcd6ce680dc466636b15
Brandon Rhodes <brhodes@signifyhealth.com>
2025-06-04 09:50:06 -0500

    Updated token key

--- test_ingestion.py
+++ test_ingestion.py
@@ -28,7 +28,7 @@ DATABASE_CONFIG = {
 BASE_API_URL = "https://api.moveworks.ai/export/v1beta2/records/conversations"
 TABLE_NAME = os.getenv('TABLE_NAME', 'sigi.conversations')
 HEADERS = {
-    'Authorization': os.getenv('API_TOKEN'),
+    'token': os.getenv('API_TOKEN'),
     'Content-Type': 'application/json'
 }
 
